300bf0fe300d45a25c01f370bb998d0e0b6d63d2
MuriloMoraes <57009618+MuriloMoraes@users.noreply.github.com>
2020-03-11 19:12:31 -0300

    Create exercicio32.java

---
+++ exercicio32.java
@@ -0,0 +1,9 @@
+public class exercicio32
+{
+   public static void main(String[] args){
+       int cont = 1;
+       while (cont < 501){
+           System.out.print(""+cont++ +" ");
+        }
+    }
+}
